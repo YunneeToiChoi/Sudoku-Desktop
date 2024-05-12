2f540f4555336ca79b0942e9f0d6236e52a65e0d
GiapKun <trandinhgiap8051@gmail.com>
2024-05-12 12:21:02 +0700

    Done create auth

---
+++ src/main/java/Database/DatabaseConnector.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Tran Giap
+ */
+public class DatabaseConnector {
+
+ private static final String URL = "jdbc:sqlserver://GIAP:1433;databaseName=test;user=sa;password={Q;`J;'[K]ya't`e)EDct};encrypt=true;trustServerCertificate=true;";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+}
